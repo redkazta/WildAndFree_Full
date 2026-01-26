d1e41f8b132801fcc2717e81ef631fea3d05d6bb
Angel castaneda <cuentaunospoti@gmail.com>
2026-01-25 23:22:03 -0600

    UI polish: sidebar + red accents

--- apps/wild-and-free-web/src/components/sidebarStore.js
+++ apps/wild-and-free-web/src/components/sidebarStore.js
@@ -1,14 +1,19 @@
 export const sidebarStore = {
   isOpen: false,
+  scrollbarWidth: 0,
   
   open() {
     this.isOpen = true;
+    const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
+    this.scrollbarWidth = scrollbarWidth;
     document.documentElement.style.overflow = 'hidden';
+    document.documentElement.style.paddingRight = scrollbarWidth > 0 ? `${scrollbarWidth}px` : '';
   },
   
   close() {
     this.isOpen = false;
     document.documentElement.style.overflow = '';
+    document.documentElement.style.paddingRight = '';
   },
   
   toggle() {
