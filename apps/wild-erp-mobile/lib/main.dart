22f4bae1ba45a7562e278d3d9423c639424ca30c
Angel Castañeda <redkazta@gmail.com>
2026-06-28 11:40:25 -0600

    Cleanup comment

--- apps/wild-erp-mobile/lib/main.dart
+++ apps/wild-erp-mobile/lib/main.dart
@@ -10,7 +10,6 @@ import 'ui/providers/orders_provider.dart';
 import 'ui/providers/inventory_provider.dart';
 import 'ui/providers/content_provider.dart';
 import 'ui/providers/users_provider.dart';
-// force rebuild: gradle.properties cleaned
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
